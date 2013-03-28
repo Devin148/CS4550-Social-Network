b3d1fea8b74217bb7a986d849e4fcbb76feda435
Devin148 <devinhurd@gmail.com>
2013-03-28 12:36:57 -0400

    Add comments for clarity

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 
+// Example definitions for database name, username, password
 define("MYSQL_HOST", "localhost");
 define("MYSQL_USER", "local_user");
 define("MYSQL_PASS", "local_pass");
 define("MYSQL_DB", "social_network");
-
+// Example crypt salt
 define("SALT" , "X3fGDs9#1");
