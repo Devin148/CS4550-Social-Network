468673b97dfeaba584210fd78db5968670bff63f
Devin148 <devinhurd@gmail.com>
2013-03-31 12:22:39 -0400

    Clean up redirect

--- login.php
+++ login.php
@@ -11,18 +11,6 @@ if (!isset($_POST["email"]) || !isset($_POST["password"])) {
 $email = $_POST["email"];
 $pass = $_POST["password"];
 
-print $email;
-print "<br />";
-print $pass;
-print "<br /><br />";
-
-print "Salt: " . SALT;
-print "<br /><br />";
-
-$hash = crypt($pass, SALT);
-print "Hashed Pass: " . $hash;
-print "<br /><br />";
-
 if ($stmt = $mysqli->prepare("SELECT EXISTS(SELECT 1 FROM users WHERE email=? AND password=?)")) {
     $stmt->bind_param('ss', $email, $hash);
     $stmt->execute();
@@ -30,10 +18,13 @@ if ($stmt = $mysqli->prepare("SELECT EXISTS(SELECT 1 FROM users WHERE email=? AN
     $stmt->fetch();
 
     if ($exists) {
-        print "Logged in.";
+        // Start the session and add some vars
         session_start();
         $_SESSION["logged_in"] = 1;
         $_SESSION["email"] = $email;
+
+        // Redirect the user to the newsfeed
+        header("Location: newsfeed.php");
     } else {
         print "No user with that email and password found.";
     }
