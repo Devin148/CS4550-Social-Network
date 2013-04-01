8cba6a158d45dbb97072ced612fe0f2fd0210667
devin148 <devinhurd@gmail.com>
2013-04-01 17:38:01 -0400

    Add case for non-existant user

--- functions.php
+++ functions.php
@@ -2,12 +2,16 @@
 
 include ("User.php");
 
+// Return a User object for the user with the given email
 function getUser($email) {
 	if (userExists($email)) {
 		return new User($email);
+	} else {
+		return null;
 	}
 }
 
+// Does a user exist with the given email?
 function userExists($email) {
 	return true;
 }
