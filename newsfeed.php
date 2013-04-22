869634989016112a6f8bf26cc47193ac592ee7fe
Devin148 <devinhurd@gmail.com>
2013-04-22 17:02:16 -0400

    Limit people you may know to those who aren't friends

--- newsfeed.php
+++ newsfeed.php
@@ -132,9 +132,11 @@ $email = $_SESSION["email"];
 
                 // Find the user and fill in the vars
                 if ($stmt = $mysqli->prepare("SELECT first_name, last_name, email
-                                              FROM users WHERE id!=? ORDER BY RAND() LIMIT 20")) {
+                                              FROM users WHERE id!=?
+                                                         AND id NOT IN (SELECT friend from friends_with WHERE user=?)
+                                              ORDER BY RAND() LIMIT 20")) {
 
-                    $stmt->bind_param('s', $user->getId());
+                    $stmt->bind_param('ss', $user->getId(), $user->getId());
                     $stmt->execute();
                     $stmt->bind_result($rand_first, $rand_last, $rand_email);
                     
