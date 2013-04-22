038f0315f0c5f96c1c35d7ce3d09c06784ca5721
unknown <d.hurd@WFA1803731906AF.nunet.neu.edu>
2013-04-22 15:41:05 -0400

    Rename friends to following

--- sidebar.php
+++ sidebar.php
@@ -4,7 +4,7 @@
             <li><img src="images/default_16.png" class="nav-fui" />Profile</li>
         </a>
         <a href="newsfeed.php"><li><i class="fui-new-16 nav-fui"></i>Newsfeed</li></a>
-        <a href="friends.php"><li><i class="fui-man-16 nav-fui"></i>Friends</li></a>
+        <a href="friends.php"><li><i class="fui-man-16 nav-fui"></i>Following</li></a>
         <a href="coops.php"><li><i class="fui-heart-16 nav-fui"></i>Co-ops</li></a>
         <a href="settings.php"><li><i class="fui-settings-16 nav-fui"></i>Settings</li></a>
         <a href="logout.php"><li><i class="fui-cross-16 nav-fui"></i>Logout</li></a>
