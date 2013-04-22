705346c91a9c7048afda1563cc91aff62a1991c6
unknown <d.hurd@WFA1803731906AF.nunet.neu.edu>
2013-04-22 15:40:40 -0400

    Add social media buttons to navbar

--- navbar.php
+++ navbar.php
@@ -1,5 +1,33 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=210781715629538";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
 <div id="navbar">
     <ul>
         <li><a href="newsfeed.php">Northeastern Social</a>
     </ul>
+	<div style="float: right; margin-top: 5px;">
+    	<!-- Place this tag where you want the +1 button to render. -->
+		<div class="g-plusone" data-size="medium"></div>
+		<!-- Place this tag after the last +1 button tag. -->
+		<script type="text/javascript">
+		  (function() {
+		    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+		    po.src = 'https://apis.google.com/js/plusone.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+		  })();
+		</script>
+    </div>
+    <div style="float: right; margin-top: 5px; margin-right: 15px;">
+    	<div class="fb-like" data-href="https://www.facebook.com/NortheasternSocial" data-send="true" data-layout="button_count" data-width="450" data-show-faces="false"></div>
+	</div>
+    <div style="float: right; margin-top: 5px; margin-right: 15px;">
+    	<a href="https://twitter.com/intent/tweet?button_hashtag=NEUSocial" class="twitter-hashtag-button">Tweet #NEUSocial</a>
+		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+	</div>
 </div>
